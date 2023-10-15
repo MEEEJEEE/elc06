2bd17bfd3c8c5ece352303e952103c5c52451f05
miji <mijilee0413@sookmyung.ac.kr>
2023-10-15 17:39:19 +0900

    6-3

--- 3main.c
+++ 3main.c
@@ -3,37 +3,8 @@
 
 //실습3
 
-int sumTwo(int a, int b) {
-    return a + b;
+int main(int argc, char *argv[]) {
+	printf("hello world!\n");
+	return 0;
 }
 
-int square(int n) {
-    return n * n;
-}
-
-int get_max(int x, int y) {
-    if (x > y) {
-        return x;
-    } else {
-        return y;
-    }
-}
-
-int main(void) {
-    int num1 = 5;
-    int num2 = 7;
-
-
-    int sum = sumTwo(num1, num2);
-    printf("Sum of the two integers: %d\n", sum);
-
-
-    int squared = square(num1);
-    printf("Square of the first integer: %d\n", squared);
-
-
-    int max_num = get_max(num1, num2);
-    printf("Greater of the two integers: %d\n", max_num);
-
-    return 0;
-}
